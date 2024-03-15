1cb8c7da1fe4aed3f2cd89acbf595224a57e1704
MartynasRB <115002251+MartynasRB@users.noreply.github.com>
2024-03-15 12:44:32 +0100

    Update custom.markers.js

--- custom.markers.js
+++ custom.markers.js
@@ -38,43 +38,43 @@ Use the imageScale property if the pin image is too large.
 
 UnminedCustomMarkers = {
 
-    isEnabled: false,
+    isEnabled: true,
 
     markers: [
 
-        // Example 1: Simple marker
-        {
-            x: -200,
-            z: -200,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-        },
+        // // Example 1: Simple marker
+        // {
+        //     x: -293,
+        //     z: 398,
+        //     image: "custom.pin.png",
+        //     imageAnchor: [0.5, 1],
+        //     imageScale: 0.1,
+        // },
 
         // Example 2: Marker with text
         {
-            x: 0,
-            z: 0,
+            x: -293,
+            z: 398,
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-            text: "Marker with text",
+            imageScale: 0.2,
+            text: "Construction Area",
             textColor: "red", 
             offsetX: 0,
             offsetY: 20,
             font: "bold 20px Calibri,sans serif",
         },
 
-        // Example 3: Text only
-        {
-            x: 200,
-            z: 200,
-            text: "Text only", 
-            textColor: "yellow", 
-            offsetX: 0,
-            offsetY: 0, 
-            font: "bold 50px Calibri,sans serif",
-        },
+        // // Example 3: Text only
+        // {
+        //     x: 200,
+        //     z: 200,
+        //     text: "Text only", 
+        //     textColor: "yellow", 
+        //     offsetX: 0,
+        //     offsetY: 0, 
+        //     font: "bold 50px Calibri,sans serif",
+        // },
 
         // add your markers here
 
